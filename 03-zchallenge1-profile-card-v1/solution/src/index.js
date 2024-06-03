edc0ab081fa5901171237584191a1c78a56a0662
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-06-03 10:57:29 +0200

    after challenege 1

--- 03-zchallenge1-profile-card-v1/solution/src/index.js
+++ 03-zchallenge1-profile-card-v1/solution/src/index.js
@@ -5,20 +5,20 @@ import "./index.css";
 function App() {
   return (
     <div className="card">
-      <Avatar
-        className="avatar"
-        source="https://static.vecteezy.com/system/resources/previews/036/594/092/non_2x/man-empty-avatar-photo-placeholder-for-social-networks-resumes-forums-and-dating-sites-male-and-female-no-photo-images-for-unfilled-user-profile-free-vector.jpg"
-      />
+      <Avatar source="https://miro.medium.com/v2/resize:fit:1187/1*0FqDC0_r1f5xFz3IywLYRA.jpeg" />
       <div className="data">
-        <Intro name="Black White" description="Lorem Ipsum" />
-        <SkillList className="skill-list" />
+        <Intro
+          name="Black White"
+          description="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
+        />
+        <SkillList />
       </div>
     </div>
   );
 }
 
 function Avatar(props) {
-  return <img src={props.source} alt="Avatar" />;
+  return <img className="avatar" src={props.source} alt="Avatar" />;
 }
 
 function Intro(props) {
@@ -32,20 +32,20 @@ function Intro(props) {
 
 function Skill(props) {
   return (
-    <div style={{ backgroundColor: props.bgColor }}>
-      <p>{props.skill}</p>
-      <p>{props.emoji}</p>
+    <div className="skill" style={{ backgroundColor: props.bgColor }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
     </div>
   );
 }
 
 function SkillList() {
   return (
-    <div>
-      <Skill className="skill" skill="first-skill" emoji="💪" bgColor="red" />
-      <Skill className="skill" skill="second-skill" emoji="💪" bgColor="blue" />
-      <Skill className="skill" skill="third-skill" emoji="💪" bgColor="green" />
-      <Skill className="skill" skill="fourth-skill" emoji="💪" bgColor="pink" />
+    <div className="skill-list">
+      <Skill skill="first-skill" emoji="💪" bgColor="red" />
+      <Skill skill="second-skill" emoji="💪" bgColor="cyan" />
+      <Skill skill="third-skill" emoji="💪" bgColor="green" />
+      <Skill skill="fourth-skill" emoji="💪" bgColor="pink" />
     </div>
   );
 }
