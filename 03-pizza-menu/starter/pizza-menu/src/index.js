3d3b79efd5d6167fba8a069263a60c9c535d168a
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-06-03 13:49:49 +0200

    conditional classnames

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -131,15 +131,16 @@ function Order({ closeHour }) {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    // <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+    <li className={!pizzaObj.soldOut ? "pizza" : "pizza sold-out"}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}$</span>
+        <span>{pizzaObj.soldOut ? "SOLD-OUT" : pizzaObj.price$}</span>
       </div>
     </li>
   );
