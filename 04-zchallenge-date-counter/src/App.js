aeacebd76501b3c53b934ab9df463c10dc6e4b86
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-06-04 15:01:11 +0200

    date counter part 2

--- 04-zchallenge-date-counter/src/App.js
+++ 04-zchallenge-date-counter/src/App.js
@@ -9,19 +9,43 @@ function App() {
     if (step > 1) setStep((s) => s - 1);
   }
 
+  function handleDayAdd() {
+    setDays((d) => {
+      const newDays = d + step;
+      const newDate = new Date();
+      newDate.setDate(newDate.getDate() + newDays);
+      setDate(newDate);
+      return newDays;
+    });
+  }
+
+  function handleDaySubstract() {
+    setDays((d) => {
+      const newDays = d - step;
+      const newDate = new Date();
+      newDate.setDate(newDate.getDate() + newDays);
+      setDate(newDate);
+      return newDays;
+    });
+  }
+
   return (
     <div className="container">
-      <div className="steps">
+      <div className="control">
         <button onClick={handleStepSubstract}>-</button>
         <span>Step: {step}</span>
         <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
-      <div className="days">
-        <button onClick={() => setDays((d) => d - step)}>-</button>
+      <div className="control">
+        <button onClick={handleDaySubstract}>-</button>
         <span>Days: {days}</span>
-        <button onClick={() => setDays((d) => d + step)}>+</button>
+        <button onClick={handleDayAdd}>+</button>
       </div>
-      <p className="date">{date.toDateString()}</p>
+      <p className="date">
+        {days === 0
+          ? `Today is ${date.toDateString()}`
+          : `${days} days from today is ${date.toDateString()}`}
+      </p>
     </div>
   );
 }
