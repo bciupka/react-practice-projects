dbf1c3652c486fd9a4fdbad17f87ab25a315b9ec
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-06-04 20:14:05 +0200

    far away project static layout

---
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What you need for your 💘 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, with packed X of them (X%)</em>
+    </footer>
+  );
+}
