2ef2c54a9a457879e9f380304fa9ce29d912cf56
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-06-18 20:28:55 +0200

    parametrized star rating with props

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieScore, setMovieScore] = useState(0);
+  return (
+    <>
+      <StarRating
+        size="30px"
+        color="magenta"
+        maxRating={10}
+        onSetRating={setMovieScore}
+      />
+      <span style={{ color: "magenta" }}>Current score - {movieScore}</span>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating />
+    <StarRating
+      size="40px"
+      color="blue"
+      className="something"
+      defaultRating={2}
+      messages={["Sucks", "Bad", "Watchable", "Nice", "Fabulous"]}
+    />
+    <Test />
   </React.StrictMode>
 );
