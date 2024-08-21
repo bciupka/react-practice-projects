ec32106b2ab2c96dc9538480e4f7dcb9e1a25def
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-08-21 14:22:23 +0200

    useReducer for related states (step and count)

--- 10-react-quiz/starter/react-quiz/src/DateCounter.js
+++ 10-react-quiz/starter/react-quiz/src/DateCounter.js
@@ -1,15 +1,27 @@
 import { useReducer } from "react";
-import { useState } from "react";
+
+const initialState = { step: 1, count: 0 };
 
 function reducer(state, action) {
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "set") return action.payload;
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
 function DateCounter() {
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { step, count } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -24,16 +36,15 @@ function DateCounter() {
   };
 
   const defineCount = function (e) {
-    dispatch({ type: "set", payload: +e.target.value });
+    dispatch({ type: "setCount", payload: +e.target.value });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    // setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
