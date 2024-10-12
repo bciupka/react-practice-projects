0eb917c4744978ed38b82580094030721b1e93ff
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-10-12 12:04:41 +0200

    challenge - bank account init

---
+++ 10-zchallenge-bank-account/src/App.js
@@ -0,0 +1,61 @@
+import "./styles.css";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return {
+        ...state,
+        balance: 500,
+        isActive: true,
+      };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: X</p>
+      <p>Loan: X</p>
+
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Open account
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Close account
+        </button>
+      </p>
+    </div>
+  );
+}
