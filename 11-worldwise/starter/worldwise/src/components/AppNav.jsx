9b250fef93d5a6784e93ae07327ea8defaef8385
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-10-16 11:45:22 +0200

    added test css modules

---
+++ 11-worldwise/starter/worldwise/src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return <div className={styles.nav}>Temporary Nav Bar for future App</div>;
+}
+
+export default AppNav;
--- 11-worldwise/starter/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/starter/worldwise/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h2>Main page</h2>
     </div>
   );
