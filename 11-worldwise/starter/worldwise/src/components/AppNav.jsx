578d6fa5021c0e6aa93923306f9422f998f3c7e9
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2024-12-10 20:03:28 +0100

    wordwise - nested routing and outlet

--- 11-worldwise/starter/worldwise/src/components/AppNav.jsx
+++ 11-worldwise/starter/worldwise/src/components/AppNav.jsx
@@ -1,7 +1,17 @@
 import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 function AppNav() {
-  return <div className={styles.nav}>Temporary Nav Bar for future App</div>;
+  return (
+    <div className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <NavLink to="countries">Countries</NavLink>
+      </ul>
+    </div>
+  );
 }
 
 export default AppNav;
