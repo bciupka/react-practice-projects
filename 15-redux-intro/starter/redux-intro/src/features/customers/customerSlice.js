d5d03fec159de5aca27d03406b513365536e55f5
Bartlomiej Ciupka <bartlomiej.ciupka@tdj.pl>
2025-04-03 13:32:36 +0200

    changed the directory structure - features as directories

---
+++ 15-redux-intro/starter/redux-intro/src/features/customers/customerSlice.js
@@ -0,0 +1,36 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function reducerCustomer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateCustomerName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+function updateCustomerName(newName) {
+  return { type: "customer/updateCustomerName", payload: newName };
+}
