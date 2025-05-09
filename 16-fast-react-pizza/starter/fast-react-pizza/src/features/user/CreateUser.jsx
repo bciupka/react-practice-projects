8719b3bdfa995980f2224384cdb1f37c857fffbb
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-05-09 21:39:11 +0200

    tailwind styling - first steps

--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/user/CreateUser.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/user/CreateUser.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+    <form className="text-center" onSubmit={handleSubmit}>
+      <p className="mb-8 text-sm md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
-      {username !== '' && (
+      {username !== "" && (
         <div>
           <button>Start ordering</button>
         </div>
--- 16-fast-react-pizza/starter/fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <div className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:p-6 sm:px-6">
+      <Link className="tracking-[5px]" to="/">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </div>
   );
 }
