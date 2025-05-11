ce648abc2f60ad3ed9bb61d01ca96f0e1bda6bcf
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-05-11 19:57:34 +0200

    tailwinf styling - more styling - buttons, forms, responsive etc

--- 16-fast-react-pizza/starter/fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:p-6 sm:px-6">
+    <div className="flex items-center justify-between border-b border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:p-6 sm:px-6">
       <Link className="tracking-[5px]" to="/">
         Fast React Pizza Co.
       </Link>
