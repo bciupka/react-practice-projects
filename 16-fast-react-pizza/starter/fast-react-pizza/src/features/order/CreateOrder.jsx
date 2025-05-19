f828691797b5c97c9b7b2ff3fc872008bb469467
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-05-19 20:41:29 +0200

    after styling the whole thing

--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -41,31 +41,40 @@ function CreateOrder() {
   const actionErrors = useActionData();
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
-            {actionErrors?.phone && <p>{actionErrors.phone}</p>}
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {actionErrors?.phone && (
+              <p className="mx-1 mt-2 rounded-md bg-red-200 p-1.5 text-xs text-red-700">
+                {actionErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className="input" type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-1"
             type="checkbox"
@@ -74,7 +83,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-semibold" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
         <input type="hidden" value={JSON.stringify(cart)} name="cart" />
 
