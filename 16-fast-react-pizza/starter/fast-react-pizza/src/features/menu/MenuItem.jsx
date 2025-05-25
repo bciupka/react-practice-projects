9b9b18ff88589f9f16b4a6efa7d41585ca17051d
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-05-25 20:45:54 +0200

    added cart state and adding new item

--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,9 +1,22 @@
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
 
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice,
+    };
+    dispatch(addItem(newItem));
+  }
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -24,7 +37,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button onClick={handleAddToCart} type="small">
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
