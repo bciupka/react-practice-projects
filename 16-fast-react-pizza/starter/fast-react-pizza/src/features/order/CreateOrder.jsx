85a63f40dc6eb469917cffb6a4ee45981c502d76
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-06-05 21:38:57 +0200

    added geolocation thunk

--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -4,9 +4,10 @@ import { formatCurrency } from "../../utils/helpers";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import EmptyCart from "../cart/EmptyCart";
 import { clearCart, getCart, getTotalPrice } from "../cart/cartSlice";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -18,6 +19,7 @@ function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+  const dispatch = useDispatch();
   const cart = useSelector(getCart);
   const actionErrors = useActionData();
   const username = useSelector((state) => state.user.username);
@@ -30,6 +32,7 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+      <button onClick={() => dispatch(fetchAddress())}>pos</button>
 
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- 16-fast-react-pizza/starter/fast-react-pizza/src/features/user/userSlice.js
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/user/userSlice.js
@@ -1,9 +1,11 @@
-// import { getAddress } from "../../services/apiGeocoding";
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
+
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
 
 // async function fetchAddress() {
 //   // 1) We get the user's geolocation position
@@ -20,10 +22,29 @@
 //   // 3) Then we return an object with the data that we are interested in
 //   return { position, address };
 // }
-import { createSlice } from "@reduxjs/toolkit";
+
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    return { position, address };
+  },
+);
 
 const initialState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -33,6 +54,21 @@ const userSlice = createSlice({
     updateName(state, action) {
       state.username = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      });
   },
 });
 
