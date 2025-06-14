27c81246b8491744c07b0a4305ddb4e775a5b3ab
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-06-15 01:04:17 +0200

    after supabase init

---
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Could not get cabins data");
+  }
+
+  return data;
+}
