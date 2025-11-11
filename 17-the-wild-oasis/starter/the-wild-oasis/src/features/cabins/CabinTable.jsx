b4b4ecb4eef943fe2d63bd596ce6c02e1e519645
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-11-11 18:44:08 +0100

    added client side sorting (name sorting bug because of number / string names) - JavaScript xd

--- 17-the-wild-oasis/starter/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -51,6 +51,13 @@ function CabinTable() {
       filteredCabins = cabins;
   }
 
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -63,7 +70,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
