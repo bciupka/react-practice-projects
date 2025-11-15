6cffcda6312e36c0e850e14c907d00c5ba2186ae
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-11-15 19:33:44 +0100

    added booking table

--- 17-the-wild-oasis/starter/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 // const Table = styled.div`
 //   border: 1px solid var(--color-grey-200);
@@ -32,6 +33,8 @@ import { useSearchParams } from "react-router-dom";
 function CabinTable() {
   const { cabins, isLoading } = useCabins();
   const [searchParams] = useSearchParams();
+
+  if (!cabins.length) return <Empty resource="cabins" />;
 
   if (isLoading) return <Spinner />;
 
