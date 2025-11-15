a1afc92e087159f5dcbd2c7e18ac5f0a57404939
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-11-15 21:00:50 +0100

    added api side filtering for bookings

--- 17-the-wild-oasis/starter/the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,10 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const { data, error, isLoading } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { bookings: data, isLoading, error };
