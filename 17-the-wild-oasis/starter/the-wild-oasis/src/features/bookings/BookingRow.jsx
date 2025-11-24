4a6ca8f040ab11a98ff9d702731ba67edbbbceeb
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-11-24 21:30:07 +0100

    added checkin feature

--- 17-the-wild-oasis/starter/the-wild-oasis/src/features/bookings/BookingRow.jsx
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/bookings/BookingRow.jsx
@@ -8,6 +8,7 @@ import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import Menus from "../../ui/Menus";
 import { useNavigate } from "react-router-dom";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -85,9 +86,20 @@ function BookingRow({
       <Menus.Menu>
         <Menus.Toggle id={bookingId} />
         <Menus.List id={bookingId}>
-          <Menus.Button onClick={() => navigate(`${bookingId}`)}>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`${bookingId}`)}
+          >
             Show details
           </Menus.Button>
+          {status === "unconfirmed" && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check-in
+            </Menus.Button>
+          )}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
---
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/check-in-out/useCheckin.js
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export default function useCheckin() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate: checkin, isLoading: isChecking } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "checked-in",
+        isPaid: true,
+      }),
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} checked-in successfully`);
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+    onError: () => {
+      toast.error("There was an error while checking-in the booking");
+    },
+  });
+
+  return { checkin, isChecking };
+}
