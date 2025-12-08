3de2aaa0d7e757048309bb2ac55ee78220a210fa
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-12-08 19:26:57 +0100

    added protected route mechanism

--- 17-the-wild-oasis/starter/the-wild-oasis/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/starter/the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,18 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate, isLoading } = useMutation({
     mutationFn: ({ email, password }) => login({ email, password }),
     onSuccess: (data) => {
+      queryClient.setQueriesData(["user"], data);
       navigate("/dashboard");
-      console.log(data);
     },
     onError: (err) => {
-      console.log("ERROR", err);
+      console.error("ERROR", err);
       toast.error("Provided email or password are incorrect");
     },
   });
